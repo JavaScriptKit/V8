294139c957f40832f636b3d282a50d83ee9cb11b
Tony Freeman <tonyfreeman@users.noreply.github.com>
2019-06-01 12:05:05 +0000

    [Cleanup] Delete copyright notice

--- Sources/CV8/include/c_v8.h
+++ Sources/CV8/include/c_v8.h
@@ -1,14 +1,3 @@
-/******************************************************************************
- *                                                                            *
- * Tris Foundation disclaims copyright to this source code.                   *
- * In place of a legal notice, here is a blessing:                            *
- *                                                                            *
- *     May you do good and not evil.                                          *
- *     May you find forgiveness for yourself and forgive others.              *
- *     May you share freely, never taking more than you give.                 *
- *                                                                            *
- ******************************************************************************/
-
 #ifndef c_v8_h
 #define c_v8_h
 
